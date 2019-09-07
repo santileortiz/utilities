8813c2dc5e4634a9e1cb0c0083f70b49a0876933
Santiago León O <santileortiz@gmail.com>
2019-09-07 17:33:27 -0500

    Add a common_unused.h file where unused but working functions will go
    I want common.h to be as small and useful as possible, if I see
    something isn't useful in general I migh put it in this file and remove
    it from common.h. Still this code should work, in case it proves to be
    useful and we want it back.

---
+++ common_unused.h
@@ -0,0 +1,77 @@
+/*
+ * Copiright (C) 2019 Santiago León O.
+ */
+
+// This file contains functions that I wrote at some point and seemed useful but
+// I ended up not using. If they prove to actually be useful then we might move
+// them into common.h.
+
+#if !defined(COMMON_UNUSED_H)
+
+off_t get_fd_size (int fd)
+{
+    assert (fd >= 0);
+    off_t stat;
+
+    off_t curr = lseek (fd, 0, SEEK_CUR);
+    assert (stat != -1);
+
+    stat = lseek (fd, 0, SEEK_SET);
+    printf ("Error: %s \n", strerror(errno));
+    assert (stat == 0);
+
+    off_t size = lseek (fd, 0, SEEK_END);
+    assert (stat != -1);
+
+    stat = lseek (fd, 0, SEEK_SET);
+    assert (stat == curr);
+
+    return size;
+}
+
+char* full_fd_read (mem_pool_t *pool, int fd)
+{
+    assert (fd >= 0);
+
+    bool success = true;
+
+    mem_pool_temp_marker_t mrk;
+    if (pool != NULL) {
+        mrk = mem_pool_begin_temporary_memory (pool);
+    }
+
+    char *loaded_data = NULL;
+    off_t size = get_fd_size (fd);
+    loaded_data = (char*)pom_push_size (pool, size + 1);
+
+    if (fd != -1) {
+        int bytes_read = 0;
+        do {
+            int status = read (fd, loaded_data+bytes_read, size);
+            if (status == -1) {
+                success = false;
+                printf ("Error reading fd: %d: %s\n", fd, strerror(errno));
+                break;
+            }
+            bytes_read += status;
+        } while (success && bytes_read != size);
+        loaded_data[size] = '\0';
+    }
+
+    char *retval = NULL;
+    if (success) {
+        retval = loaded_data;
+    } else if (loaded_data != NULL) {
+        if (pool != NULL) {
+            mem_pool_end_temporary_memory (mrk);
+        } else {
+            free (loaded_data);
+        }
+    }
+
+    return retval;
+}
+
+
+#define COMMON_UNUSED_H
+#endif
